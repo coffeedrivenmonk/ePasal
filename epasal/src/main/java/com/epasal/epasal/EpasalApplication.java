c6bbae4e384a31fe1ffbb861a4aae518702d1266
Biplope <maharjanbiplope16@gmail.com>
2022-12-05 11:14:41 +0545

    initilization

---
+++ epasal/src/main/java/com/epasal/epasal/EpasalApplication.java
@@ -0,0 +1,13 @@
+package com.epasal.epasal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpasalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpasalApplication.class, args);
+	}
+
+}
---
+++ epasal/src/test/java/com/epasal/epasal/EpasalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epasal.epasal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpasalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
