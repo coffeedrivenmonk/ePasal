51f1b1966f385cc5505b2de29535a12dba6a9c2d
Prahant23 <prashantbist64@gmail.com>
2023-02-23 12:29:30 +0545

    update

--- src/main/java/com/example/epasal/EPasalApplication.java
+++ src/main/java/com/example/epasal/EPasalApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EPasalApplication {
+public class EPasalApplication {agva
 
     public static void main(String[] args) {
         SpringApplication.run(EPasalApplication.class, args);
